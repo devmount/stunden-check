2367e4566de2aad634650e338d27b936e8785b2c
Andreas Müller <mail@devmount.de>
2022-09-19 22:35:25 +0200

    ➕ mockups for dashboard and profile pages

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,94 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Stundenübersicht') }}
+            </h2>
+            <x-primary-button>
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 fill-transparent stroke-current stroke-2 text-white" viewBox="0 0 24 24">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <line x1="12" y1="5" x2="12" y2="19" />
+                    <line x1="5" y1="12" x2="19" y2="12" />
+                </svg>
+                {{ __('Neuer Eintrag') }}
+            </x-primary-button>
+        </div>
     </x-slot>
 
     <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid gap-4 grid-cols-3 justify-between">
+            <div class="overflow-hidden shadow-md sm:rounded-lg p-6 text-center text-white bg-slate-600 border-4 border-white">
+                Insgesamt
+                <div class="text-6xl font-medium">35</div>
+                Stunden geleistet
+            </div>
+            <div class="overflow-hidden shadow-md sm:rounded-lg p-6 text-center text-white bg-teal-600 border-4 border-white">
+                Aktuelle Abrechnungsperiode
+                <div class="text-6xl font-medium">11</div>
+                Stunden geleistet
+            </div>
+            <div class="overflow-hidden shadow-md sm:rounded-lg p-6 text-center text-white bg-amber-600 border-4 border-white">
+                Aktuelle Abrechnungsperiode
+                <div class="text-6xl font-medium">13</div>
+                Stunden ausstehend
+            </div>
+        </div>
+    </div>
+
+    <div class="pb-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg py-6 border-b border-gray-200">
+                <div class="px-6 pb-4">Geleistete Stunden</div>
+                <table class="items-center bg-transparent w-full border-collapse">
+                    <thead>
+                        <tr>
+                            <th class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-200 py-3 uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Beschreibung
+                            </th>
+                            <th class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-200 py-3 uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Datum
+                            </th>
+                            <th class="px-6 bg-slate-50 text-slate-500 align-middle border border-solid border-slate-200 py-3 uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Anzahl Stunden
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="px-6 py-4 align-middle whitespace-nowrap text-left">
+                            Rasen gemäht auf gesamten Kita-Gelände
+                        </td>
+                        <td class="px-6 py-4 align-middle whitespace-nowrap">
+                            22. August 2022
+                        </td>
+                        <td class="px-6 py-4 align-center whitespace-nowrap">
+                            4
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-6 py-4 align-middle whitespace-nowrap text-left">
+                            Klo geputzt im Fachwerkhaus
+                        </td>
+                        <td class="px-6 py-4 align-middle whitespace-nowrap">
+                            25. August 2022
+                        </td>
+                        <td class="px-6 py-4 align-center whitespace-nowrap">
+                            2
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-6 py-4 align-middle whitespace-nowrap text-left">
+                            Holztor repariert
+                        </td>
+                        <td class="px-6 py-4 align-middle whitespace-nowrap">
+                            26. August 2022
+                        </td>
+                        <td class="px-6 py-4 align-center whitespace-nowrap">
+                            2
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,19 +1,23 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Your Profile') }}
+            {{ __('Dein Profil') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div>
-                        <label class="font-bold">Name:</label> {{ Auth::user()->name }}
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 border-b border-gray-200">
+                <div class="text-center mt-2 text-3xl font-medium">{{ Auth::user()->name }}</div>
+                <div class="text-center mt-2 font-light text-sm">{{ Auth::user()->email }}</div>
+                <hr class="mt-8">
+                <div class="flex p-4">
+                    <div class="w-1/2 text-center">
+                        {{ __('Erstellt am') }} <span class="font-bold"> {{ date("d. F Y", strtotime(Auth::user()->created_at)) }}</span>
                     </div>
-                    <div>
-                        <label class="font-bold">E-Mail:</label> {{ Auth::user()->email }}
+                    <div class="w-0 border border-gray-300"></div>
+                    <div class="w-1/2 text-center">
+                        {{ __('Gültig bis') }} <span class="font-bold">...</span>
                     </div>
                 </div>
             </div>
