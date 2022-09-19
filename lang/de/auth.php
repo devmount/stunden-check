46760323dd98940d505bbd3b487127e8ed25a9ca
Andreas Müller <mail@devmount.de>
2022-09-19 21:20:54 +0200

    🌐 add German translation

---
+++ lang/de/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Die Anmeldedaten konnten keinem Datensatz zugeordnet werden.',
+    'password' => 'Das angegebene Passwort stimmt nicht.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte in :seconds Sekunden nochmal versuchen.',
+
+];
---
+++ lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Das Passwort wurde zurückgesetzt!',
+    'sent' => 'Der Link zum Zurücksetzen des Passworts wurde versandt!',
+    'throttled' => 'Bitte mit dem nächsten Anmeldeversuch warten.',
+    'token' => 'Das Token zum Zurücksetzen des Passworts ist ungültig.',
+    'user' => "Es gibt keinen Benutzer zu dieser E-Mail-Adresse.",
+
+];
