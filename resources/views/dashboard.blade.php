25ae40db2ace70871160ac196be940850aa5dd4f
Andreas Müller <mail@devmount.de>
2022-09-23 21:02:42 +0200

    💚 improve modal component implementation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,53 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Übersicht Stunden') }}
             </h2>
-            <x-primary-button class="transition duration-150 ease-in-out" data-target-modal="new-position">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 fill-transparent stroke-current stroke-2 text-white" viewBox="0 0 24 24">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <line x1="12" y1="5" x2="12" y2="19" />
-                    <line x1="5" y1="12" x2="19" y2="12" />
-                </svg>
-                {{ __('Neuer Eintrag') }}
-            </x-primary-button>
+            {{-- dialog to add new position --}}
+            <x-modal>
+                <x-slot name="trigger">
+                    <x-primary-button class="transition duration-150 ease-in-out">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 fill-transparent stroke-current stroke-2 text-white" viewBox="0 0 24 24">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <line x1="12" y1="5" x2="12" y2="19" />
+                            <line x1="5" y1="12" x2="19" y2="12" />
+                        </svg>
+                        {{ __('Neuer Eintrag') }}
+                    </x-primary-button>
+                </x-slot>
+
+                <x-slot name="title">
+                    Stunden schreiben
+                </x-slot>
+
+                <div class="mb-4">Gib hier an, wie lang und was du gearbeitet hast.</div>
+
+                <form id="new-position-form" method="POST" action="{{ route('dashboard') }}">
+                    @csrf
+
+                    <!-- date (no time needed) -->
+                    <div>
+                        <x-input-label for="date" :value="__('Datum')" />
+                        <x-text-input id="date" class="block mt-1 w-full" type="date" name="date" :value="old('date')" required autofocus />
+                    </div>
+
+                    <!-- hours -->
+                    <div class="mt-4">
+                        <x-input-label for="hours" :value="__('Stundenanzahl')" />
+                        <x-text-input id="hours" class="block mt-1 w-full" type="number" name="hours" :value="old('hours')" required autofocus />
+                    </div>
+
+                    <!-- description -->
+                    <div class="mt-4">
+                        <x-input-label for="description" :value="__('Beschreibung der Tätigkeit')" />
+                        <x-text-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required autofocus />
+                    </div>
+                </form>
+
+                <x-slot name="action">
+                    <x-primary-button onclick="event.preventDefault();document.getElementById('new-position-form').submit();">
+                        {{ __('Speichern') }}
+                    </x-primary-button>
+                </x-slot>
+            </x-modal>
         </div>
     </x-slot>
 
@@ -92,16 +131,4 @@
             </div>
         </div>
     </div>
-    {{-- dialog to add new position --}}
-    <x-modal id="new-position">
-        <x-slot name="title">
-            Stunden schreiben
-        </x-slot>
-        Hier kann eine neue Position angelegt werden.
-        <x-slot name="action">
-            <x-primary-button>
-                {{ __('Speichern') }}
-            </x-primary-button>
-        </x-slot>
-    </x-modal>
 </x-app-layout>
