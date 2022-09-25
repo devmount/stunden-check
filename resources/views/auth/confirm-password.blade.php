9913e8dd8f53bad6a230195921f648cbd1577175
Andreas Müller <mail@devmount.de>
2022-09-25 11:22:49 +0200

    🌐 add translations

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -8,7 +8,7 @@
 		</x-slot>
 
 		<div class="mb-4 text-sm text-gray-600">
-			{{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+			{{ __('Das ist ein geschützter Bereich. Zum Fortfahren bitte bestätige mit deinem Passwort.') }}
 		</div>
 
 		<!-- Validation Errors -->
@@ -29,7 +29,7 @@
 
 			<div class="flex justify-end mt-4">
 				<x-primary-button>
-					{{ __('Confirm') }}
+					{{ __('Bestätigen') }}
 				</x-primary-button>
 			</div>
 		</form>
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -8,31 +8,27 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            {{ __('Danke für die Registrierung! Bevor es losgehen kann, verifiziere bitte deine E-Mail-Adresse in dem du auf den Link klickst, der dir soeben gesendet wurde. Falls die E-Mail nicht angekommen ist, senden wir dir gern einen neuen Link zu.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                {{ __('Ein neuer Verifizierungslink wurde an die bei der Registrierung angegebenen E-Mail-Adresse versendet.') }}
             </div>
         @endif
 
         <div class="mt-4 flex items-center justify-between">
             <form method="POST" action="{{ route('verification.send') }}">
                 @csrf
-
-                <div>
-                    <x-primary-button>
-                        {{ __('Resend Verification Email') }}
-                    </x-primary-button>
-                </div>
+                <x-primary-button>
+                    {{ __('Link erneut versenden') }}
+                </x-primary-button>
             </form>
 
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
-
                 <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
+                    {{ __('Abmelden') }}
                 </button>
             </form>
         </div>
