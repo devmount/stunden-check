ab100fad6e474d1176aeee2ff9474a5c29bbe571
Andreas Müller <mail@devmount.de>
2022-10-08 06:36:55 +0200

    ➕ add admin positions seeding

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Account;
+use App\Models\User;
+use App\Models\Position;
 
 class AdminSeeder extends Seeder
 {
@@ -17,23 +20,33 @@ class AdminSeeder extends Seeder
 	public function run()
 	{
 		// create first admin account
-		DB::table('accounts')->insert([
-			'active' => true,
-			'start' => now(),
-			'target_hours' => 24,
-			'separate_accounting' => false,
-			'created_at' => now(),
-		]);
+		$account = Account::factory()
+			->state([
+				'active' => true,
+				'start' => now(),
+				'target_hours' => 24,
+				'separate_accounting' => false,
+				'created_at' => now(),
+			])
+			->create();
 
-		// create corresponding user
-		DB::table('users')->insert([
-			'firstname' => 'App',
-			'lastname' => 'Admin',
-			'email' => 'admin@example.com',
-			'password' => Hash::make('Joh.3,16'),
-			'is_admin' => true,
-			'created_at' => now(),
-			'account_id' => 1,
-		]);
+		// create corresponding admin user
+		$user = User::factory()
+			->state([
+				'firstname' => 'App',
+				'lastname' => 'Admin',
+				'email' => 'admin@example.com',
+				'password' => Hash::make('Joh.3,16'),
+				'is_admin' => true,
+				'created_at' => now()
+			])
+			->for($account)
+			->create();;
+
+		// create positions for admin user
+		Position::factory()
+			->count(8)
+			->for($user)
+			->create();
 	}
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
 	{
 		$this->call([
 			ParametersSeeder::class,
-			AdminSeeder::class,
 			CategorySeeder::class,
+			AdminSeeder::class,
 			TestAccountsSeeder::class,
 		]);
 	}
