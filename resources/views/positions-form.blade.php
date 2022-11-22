5fac538533a6a51a4a58ae0f4a9b463ed0a7eae4
Andreas Müller <mail@devmount.de>
2022-11-22 21:52:04 +0100

    💚 improve section margins

--- resources/views/positions-form.blade.php
+++ resources/views/positions-form.blade.php
@@ -8,7 +8,7 @@
 		</h2>
 	</x-slot>
 
-	<div class="py-12">
+	<div class="pt-12">
 		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 			<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 border-b border-gray-200">
 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,7 +5,7 @@
 		</h2>
 	</x-slot>
 
-	<div class="py-12">
+	<div class="pt-12">
 		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 			<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 border-b border-gray-200">
 				<div class="text-center mt-2 text-3xl font-medium">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</div>
