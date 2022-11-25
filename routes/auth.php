49413d4be111a234ff502ddd97ba57bc0089261a
Andreas Müller <mail@devmount.de>
2022-11-25 13:57:10 +0100

    ❌ deactivate user registration

--- routes/auth.php
+++ routes/auth.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
+// use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\PositionController;
 use App\Http\Controllers\AccountController;
@@ -16,8 +16,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-	Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
-	Route::post('/register', [RegisteredUserController::class, 'store']);
+	// Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+	// Route::post('/register', [RegisteredUserController::class, 'store']);
 	Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
 	Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 	Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
