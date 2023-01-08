4bc24180a8670d5256bab109ef7baf0d6bc83fd4
Andreas Müller <mail@devmount.de>
2023-01-08 20:50:21 +0100

    💚 improve credits appearance

--- resources/views/forms/account-formfields.blade.php
+++ resources/views/forms/account-formfields.blade.php
@@ -129,14 +129,14 @@
 				</div>
 			</template>
 			<button
-			class="transition duration-150 ease-in-out mt-2"
-			@click="event.preventDefault();add++;"
-			>
-			<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-transparent stroke-teal-600 hover:stroke-teal-500 stroke-2" viewBox="0 0 24 24">
-				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-				<line x1="12" y1="5" x2="12" y2="19" />
-				<line x1="5" y1="12" x2="19" y2="12" />
-			</svg>
+				class="transition duration-150 ease-in-out mt-2"
+				@click="event.preventDefault();add++;"
+				>
+				<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-transparent stroke-teal-600 hover:stroke-teal-500 stroke-2" viewBox="0 0 24 24">
+					<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+					<line x1="12" y1="5" x2="12" y2="19" />
+					<line x1="5" y1="12" x2="19" y2="12" />
+				</svg>
 			</button>
 		</div>
 	</div>
@@ -220,14 +220,14 @@
 				</div>
 			</template>
 			<button
-			class="transition duration-150 ease-in-out mt-2"
-			@click="event.preventDefault();add++;"
-			>
-			<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-transparent stroke-teal-600 hover:stroke-teal-500 stroke-2" viewBox="0 0 24 24">
-				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-				<line x1="12" y1="5" x2="12" y2="19" />
-				<line x1="5" y1="12" x2="19" y2="12" />
-			</svg>
+				class="transition duration-150 ease-in-out mt-2"
+				@click="event.preventDefault();add++;"
+				>
+				<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-transparent stroke-teal-600 hover:stroke-teal-500 stroke-2" viewBox="0 0 24 24">
+					<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+					<line x1="12" y1="5" x2="12" y2="19" />
+					<line x1="5" y1="12" x2="19" y2="12" />
+				</svg>
 			</button>
 		</div>
 	</div>
--- resources/views/layouts/credits.blade.php
+++ resources/views/layouts/credits.blade.php
@@ -9,16 +9,13 @@
 				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
 				<path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
 			</svg>
-	
-			<a href="https://github.com/devmount/stunden-check" class="ml-1 text-sm underline" target="_blank">
-				Open Source auf GitHub
+			<a href="https://github.com/devmount/stunden-check" class="ml-1 text-xs underline" target="_blank">
+				{{ config('app.name', 'StundenCheck') }} auf GitHub
 			</a>
-	
 			<svg viewBox="0 0 24 24" class="stroke-current stroke-2 fill-transparent ml-4 -mt-px w-5 h-5 text-gray-400">
 				<path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
 			</svg>
-	
-			<a href="https://paypal.me/devmount" class="ml-1 text-sm underline" target="_blank">
+			<a href="https://paypal.me/devmount" class="ml-1 text-xs underline" target="_blank">
 				Spende an den Entwickler
 			</a>
 		</div>
@@ -27,7 +24,7 @@
 	<div>
 		@if (auth()->user()?->is_admin)
 			<div class="flex flex-col sm:flex-row sm:gap-2 text-sm text-gray-500">
-				<div>StundenCheck <code>v{{ config('app.version') }}</code></div>
+				<div>{{ config('app.name', 'StundenCheck') }} <code>v{{ config('app.version') }}</code></div>
 				<div class="hidden sm:block">&middot;</div>
 				<div>Laravel <code>v{{ Illuminate\Foundation\Application::VERSION }}</code></div>
 				<div class="hidden sm:block">&middot;</div>
