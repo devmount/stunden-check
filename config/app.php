610c8b55fc9d2d1ce70f3d3fde99eaef1496f3cd
Andreas Müller <mail@devmount.de>
2023-01-13 22:20:22 +0100

    ✅ v1.0.0

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ return [
 
 	'name' => env('APP_NAME', 'StundenCheck'),
 
-	'version' => env('APP_VERSION', '0.1.0'),
+	'version' => env('APP_VERSION', '1.0.0'),
 
 	/*
 	|--------------------------------------------------------------------------
