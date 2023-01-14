9121fd36f41962087667c27fb45d6126c3840b7b
Andreas Müller <mail@devmount.de>
2023-01-14 21:32:44 +0100

    🔨 replace env by config values

--- resources/views/mail/init-account-email.blade.php
+++ resources/views/mail/init-account-email.blade.php
@@ -1,10 +1,10 @@
 <p>Hallo {{ $user->firstname }} {{ $user->lastname }},</p>
 <p>es wurde soeben ein {{ config('app.name', 'StundenCheck') }} Benutzerkonto für dich angelegt. Du hast nun die Möglichkeit, die von dir geleisteten Stunden zu registrieren.</p>
-<p>Du erreichst die {{ config('app.name', 'StundenCheck') }} Webanwendung unter dieser Adresse:<br>{{ env('APP_URL') }}</p>
+<p>Du erreichst die {{ config('app.name', 'StundenCheck') }} Webanwendung unter dieser Adresse:<br>{{ config('app.url') }}</p>
 <p>Das sind deine Anmeldedaten:</p>
 <pre>
 {{ $user->email }}
 {{ $pass }}
 </pre>
-<p>Bitte ändere dein Passwort beim ersten Login. Falls du Fragen hast oder Probleme mit der Anwendung, kontaktiere uns gern unter {{ env('MAIL_FROM_ADDRESS') }}</p>
+<p>Bitte ändere dein Passwort beim ersten Login. Falls du Fragen hast oder Probleme mit der Anwendung, kontaktiere uns gern unter {{ config('mail.from.address') }}</p>
 <p>Dein {{ config('app.name', 'StundenCheck') }} Team<br>{{ $title }}</p>
