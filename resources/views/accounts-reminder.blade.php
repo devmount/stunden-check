658f739fe5d24b44ad7b53ca745f0703c0bc32df
Andreas Müller <mail@devmount.de>
2023-12-08 14:50:46 +0100

    ➕ add reminder email preview

--- resources/views/accounts-reminder.blade.php
+++ resources/views/accounts-reminder.blade.php
@@ -10,9 +10,9 @@
 
 	<div class="pt-12">
 		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-			<x-content-card class="p-6">
+			<x-content-card class="p-6 flex flex-col gap-8">
 
-				<div class="mb-8">
+				<div>
 					{{ __('An alle Konten mit ausstehenden Stunden können hier Erinnerungs-E-Mails geschickt werden.') }}<br />
 					@if ($executed)
 						{{ __('Zuletzt wurde diese Funktion am ' . hdatetime($executed) . ' ausgeführt.') }}
@@ -20,6 +20,13 @@
 						{{ __('Diese Funktion wurde bisher noch nie ausgeführt.') }}
 					@endif
 					{{ __('Aktuell gibt es ' . count($users) . ' Benutzer deren Konto ausstehende Stunden für den aktuellen Abrechnungszeitraum hat.') }}
+				</div>
+
+				<div class="flex flex-col gap-4">
+					<div>{{ __('Vorschau des E-Mail Inhalts:') }}</div>
+					<div class="mail-preview sm:w-2/3 font-mono text-xs bg-gray-100 dark:bg-gray-800/50 rounded-lg p-6">
+						@include('mail.reminder-email', App\Mail\ReminderMail::demoData())
+					</div>
 				</div>
 
 				<form method="POST" action="{{ route('accounts-reminder') }}">
